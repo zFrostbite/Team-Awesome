93a97e5cbe9c4ced824497063b09ded7815ed71a
Frostbite <Frostbite@Frostbite-PC>
2015-01-13 20:56:01 +0100

    Drive på me tutorials kandusei

--- Ruben/Tutorials.php
+++ Ruben/Tutorials.php
@@ -1,3 +1,15 @@
-<?php
+<?php 
+
+$dag = 'Torsdag';
+$dato = 13;
+$år = 2015;
+
+//   echo 'Datoen e '.$dag.' '.$dato.' '.$år;
+
+if($dag == 'Tirsdag') {
+	echo "Det e Tirsdag idag!";
+}else {
+	echo "Idag e det ikkje Tirsdag..";
+}
 
 ?>
